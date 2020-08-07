296301fa0df5875d5c53a0d9efb693877b7ee0fa
Sierra Wallace <sskwallace@gmail.com>
2020-08-07 20:09:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,8 @@ def line(katz_deli)
 
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli.push(name)
   puts ("Welcome, #{name}. You are number #{katz_deli.size} in line.")
 end
 
+def
