427a82d258ae86383a5adee1f5dc0ab9327e2c22
Sierra Wallace <sskwallace@gmail.com>
2020-08-07 19:52:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+    phrase = "The line is currently: "
+   katz_deli.each_with_index do |name, index|
+    phrase += "#{index.to_i + 1}. #{name} "
+   end
+   puts "#{phrase}" 
+ end
+ end
+ 
+
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  return ("Welcome, #{name}. You are number #{katz_deli.size} in line.")
+end
+
