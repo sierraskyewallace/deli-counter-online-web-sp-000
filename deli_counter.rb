f7fee9b11e17bd712a1de0e4beb134c7c2fa0613
Sierra Wallace <sskwallace@gmail.com>
2020-08-07 20:11:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def now_serving(katz_deli)
   if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.shift}"
+    puts "Currently serving #{katz_deli.shift}."
 end
 end
